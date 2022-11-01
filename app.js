71e5ad1915fadd3d121821b66cc2e7a4e990a197
Djany Mouton <djany.developpement@gmail.com>
2022-11-01 14:38:48 +0100

    add JS animation

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-let title = document.querySelector("#h1-header")
+// Animation header title //
+
+let h1Header = document.querySelector("#h1_header");
+const title = "trainingDev";
+let limit = title.length;
+let index = 0;
+
+const titleInterval = setInterval(() => {
+    if(index <= limit)
+    h1Header.innerText = title.slice(0,index++);
+    else{
+        clearInterval(titleInterval)
+    }
+}, 100);
+
+// Animation header menu 
+
+let headerMenu = document.querySelector("#button_list");
+console.log(headerMenu);
+const appearButton = setTimeout(() => {
+    headerMenu.classList.add("visible");
+    
+}, 1500)
